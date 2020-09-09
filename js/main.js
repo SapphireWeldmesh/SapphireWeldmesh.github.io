eee8a3f01c67034c847dd9f980e297c1d3b0e4be
siddharthsham <siddhsham@gmail.com>
2020-09-09 08:45:52 +0530

    minor fixes v0.1.0

--- js/main.js
+++ js/main.js
@@ -1,41 +1,48 @@
-function throttle(cb, ms = 0, ctx) {
-    let lastTime = 0;
-    let applyTimeout;
-    return (...args) => {
-        const now = (performance || Date).now();
-        if (lastTime && now < lastTime + ms) {
-            clearTimeout(applyTimeout);
-            applyTimeout = setTimeout(() => {
-                lastTime = now;
-                cb.apply(ctx, args);
-            }, ms);
-        } else {
-            lastTime = now;
-            cb.apply(ctx, args);
-        }
-    };
+// function throttle(cb, ms = 0, ctx) {
+//     let lastTime = 0;
+//     let applyTimeout;
+//     return (...args) => {
+//         const now = (performance || Date).now();
+//         if (lastTime && now < lastTime + ms) {
+//             clearTimeout(applyTimeout);
+//             applyTimeout = setTimeout(() => {
+//                 lastTime = now;
+//                 cb.apply(ctx, args);
+//             }, ms);
+//         } else {
+//             lastTime = now;
+//             cb.apply(ctx, args);
+//         }
+//     };
+// }
+
+window.addEventListener('DOMContentLoaded', () => {
+    window.scrollTo(0, 0)
+})
+
+window.onload = () => {
+    document.body.style.overflow = 'auto'
+    document.getElementById('loader').classList.toggle('loaded')
 }
 
 
 
 
+// let lastFrameTime = 0;
+// let tolerance = 0.1;
+// let interval = 16
+// const render = elapsedTime => {
+//     let delta = elapsedTime - (lastFrameTime || 0);
+//     let nextFrame = requestAnimationFrame(render)
+//     if (lastFrameTime && (delta <= interval - tolerance)) return
+//     lastFrameTime = elapsedTime - (delta % interval);
 
+//     try {
+//         // s.update(delta)
+//     } catch (e) {
+//         console.log('[Error in update loop]:', e)
+//         cancelAnimationFrame(nextFrame)
+//     }
+// }
 
-let lastFrameTime = 0;
-let tolerance = 0.1;
-let interval = 16
-const render = elapsedTime => {
-    let delta = elapsedTime - (lastFrameTime || 0);
-    let nextFrame = requestAnimationFrame(render)
-    if (lastFrameTime && (delta <= interval - tolerance)) return
-    lastFrameTime = elapsedTime - (delta % interval);
-
-    try {
-        // s.update(delta)
-    } catch (e) {
-        console.log('[Error in update loop]:', e)
-        cancelAnimationFrame(nextFrame)
-    }
-}
-
-requestAnimationFrame(render)
+// requestAnimationFrame(render)
